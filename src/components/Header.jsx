91f89e5288fe3af4e30242fac6e225cdd1caba43
Prajwal-tagde <prajwaltagde8@gmail.com>
2023-11-13 11:36:25 +0530

    second commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,8 +29,7 @@ const Header = () => {
                       hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300'>Pages</li>
                     <li className='text-base text-black font-bold hover:text-orange-900
                       hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300'>Shop</li>
-                    <li className='text-base text-black font-bold hover:text-orange-900
-                      hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300'>Element</li>
+  
                     <li className='text-base text-black font-bold hover:text-orange-900
                       hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300'>Blog</li>
                 </ul>
